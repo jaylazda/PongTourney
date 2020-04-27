43d498f1c83cd8ec7cde722f11d263e6c3dcc5b0
Jacob Lazda <jaylazda@gmail.com>
2020-04-27 14:49:37 -0400

    Initial Commit

---
+++ Pong/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pong
+//
+//  Created by Jacob Lazda on 2020-04-27.
+//  Copyright © 2020 Jacob Lazda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
